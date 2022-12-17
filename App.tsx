babb641fb83c2b19fe98d06fd6fb9c17b9275e60
JosiahFu <josiahfu@gmail.com>
2022-12-16 21:04:22 -0800

    Testing and experiments

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+            <Button title="AAAAAA" />
+            <Text style={styles.button}>AAAAAA</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        backgroundColor: '#f00',
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        alignItems: 'center',
+        // width: '100px',
+        // heigth: '100px',
+    }
 });
