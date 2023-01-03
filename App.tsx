2a391d5078c9dd7d4ba1f045dc531645624b69af
JosiahFu <josiahfu@gmail.com>
2023-01-02 20:14:46 -0800

    WIP testing

--- App.tsx
+++ App.tsx
@@ -1,31 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, GestureResponderEvent, TouchableNativeFeedback } from 'react-native';
 
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
-            <Button title="AAAAAA" />
-            <Text style={styles.button}>AAAAAA</Text>
-        </View>
-    );
+interface AppProps {
+}
+interface AppState {
+    menuOpen: boolean
+}
+export default class App extends React.Component<AppProps, AppState> {
+    constructor(props: any) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {menuOpen: false};
+    }
+    
+    handleClick(e: GestureResponderEvent) {
+        this.setState({menuOpen: true});
+        return true;
+    }
+
+    render() {
+        return (
+            <View>
+                <View style={[styles.sidebar, this.state.menuOpen ? styles.sidebarOpen : null]}>
+                    <Text>Column1</Text>
+                </View>
+                <View style={{height: 100}}></View>
+                <TouchableNativeFeedback onPress={this.handleClick}>
+                    <Text>Hello</Text>
+                </TouchableNativeFeedback>
+                <StatusBar style="auto" />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    button: {
-        backgroundColor: '#f00',
+    sidebar: {
+        position: 'absolute',
+        zIndex: 10,
+        left: -200,
+        top: 100,
         width: 200,
-        height: 200,
-        borderRadius: 100,
-        alignItems: 'center',
-        // width: '100px',
-        // heigth: '100px',
+        backgroundColor: 'green'
+    },
+    sidebarOpen: {
+        left: 0
     }
 });
